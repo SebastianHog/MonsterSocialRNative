cdae52b35e5f495aee8384fb647e4e8104af463d
sebastian Högsander <seblhog@gmail.com>
2024-11-22 01:13:14 +0100

    creating posts works now

--- screens/CreatePostScreen/CreatePostScreen.tsx
+++ screens/CreatePostScreen/CreatePostScreen.tsx
@@ -1,8 +1,31 @@
 import { View, Text, TextInput, Pressable } from 'react-native';
 import { styles } from './styles.js';
+import axios from 'axios';
+import { useState } from 'react';
+import { useGlobalStore } from '../../State/GlobalStore';
 
 export const CreatePostScreen = ({ navigation, route }: any) => {
-	const sendPost = () => {};
+	const [postTitle, setPostTitle] = useState('');
+	const [tags, setTags] = useState<string[]>([]);
+	const [postBody, setPostBody] = useState('');
+
+	const { state, dispatch } = useGlobalStore();
+
+	const sendPost = async () => {
+		const post = {
+			title: postTitle,
+			tags: tags,
+			body: postBody,
+			author: state.user?.username,
+		};
+
+		try {
+			const res = await axios.post('http://10.0.2.2:3000/api/posts', post);
+			console.log(res.status);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
 	return (
 		<View style={styles.container}>
@@ -11,6 +34,7 @@ export const CreatePostScreen = ({ navigation, route }: any) => {
 					style={styles.input}
 					placeholder="Post title"
 					maxLength={45}
+					onChange={(e) => setPostTitle(e.nativeEvent.text)}
 				/>
 				<View
 					id="tags"
@@ -18,22 +42,27 @@ export const CreatePostScreen = ({ navigation, route }: any) => {
 					<TextInput
 						style={styles.tagInput}
 						maxLength={8}
+						onChange={(e) => setTags([...tags, e.nativeEvent.text])}
 					/>
 					<TextInput
 						style={styles.tagInput}
 						maxLength={8}
+						onChange={(e) => setTags([...tags, e.nativeEvent.text])}
 					/>
 					<TextInput
 						style={styles.tagInput}
 						maxLength={8}
+						onChange={(e) => setTags([...tags, e.nativeEvent.text])}
 					/>
 					<TextInput
 						style={styles.tagInput}
 						maxLength={8}
+						onChange={(e) => setTags([...tags, e.nativeEvent.text])}
 					/>
 					<TextInput
 						style={styles.tagInput}
 						maxLength={8}
+						onChange={(e) => setTags([...tags, e.nativeEvent.text])}
 					/>
 				</View>
 				<TextInput
@@ -42,11 +71,12 @@ export const CreatePostScreen = ({ navigation, route }: any) => {
 					maxLength={250}
 					placeholder="Post"
 					style={[styles.input, styles.bodyInput]}
+					onChange={(e) => setPostBody(e.nativeEvent.text)}
 				/>
 			</View>
 			<Pressable
 				style={styles.postButton}
-				onPress={() => sendPost}>
+				onPress={sendPost}>
 				<Text
 					style={{
 						textAlign: 'center',
